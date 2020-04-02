1768473f3e69e38db4bb1b77eba6a3d0813d2194
Eunbyul <iks1614@gmail.com>
2020-04-02 21:42:06 +0900

    Albums 테이블 추가, Songs 테이블에 조인 컬럼 추가

--- demo/bin/main/db/migration/V2__Add_items.sql
+++ demo/bin/main/db/migration/V2__Add_items.sql
@@ -3,9 +3,11 @@ INSERT INTO Items (name, quantity) VALUES ('item2', 10);
 INSERT INTO Items (name, quantity) VALUES ('item3', 10);
 
 
-INSERT INTO Songs (title) VALUES ('title1');
-INSERT INTO Songs (title) VALUES ('title2');
-INSERT INTO Songs (title) VALUES ('title3');
+INSERT INTO Songs (title, album_id) VALUES ('title1', null);
+INSERT INTO Songs (title, album_id) VALUES ('title2', 1);
+INSERT INTO Songs (title, album_id) VALUES ('title3', 1);
+
+INSERT INTO Albums (title) VALUES ('album1');
 
 
 
